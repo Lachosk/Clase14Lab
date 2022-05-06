a26c2859ec174a6994d9f1c798af3973c79b52ab
Lachosk <nicolaxioplay@gmail.com>
2022-05-06 18:49:30 -0500

    Arreglar esto

---
+++ src/duck.ts
@@ -0,0 +1,16 @@
+class Duck {
+  constructor() {
+this.posX;
+this.posY;
+
+this.r;
+this.g;
+this.b;
+
+this.radio;
+  }
+
+public function drawDuck(posX:number,posY:number,r:number,g:number,b:number,radio:number,) {
+    
+}
+}
